80f6fa9bb8869c4d8b30978fea5dd6cfa0f13318
IkerRAD <ikerbueno30@gmail.com>
2023-06-03 00:43:36 +0200

    setup2

--- src/setup.py
+++ src/setup.py
@@ -1,14 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='vaetfg',
     version='0.1',
-    packages=['utils', 'utils.losses', 'utils.losses.images', 'utils.losses.images.application', 'utils.batches',
-              'utils.batches.domain', 'utils.batches.domain.exceptions', 'utils.batches.application',
-              'utils.batches.infrastructure', 'utils.epsilons', 'utils.epsilons.domain', 'utils.epsilons.application',
-              'utils.epsilons.infrastructure', 'utils.external', 'utils.external.fid', 'utils.external.tvd', 'project',
-              'project.domain', 'project.domain.exceptions', 'project.infrastructure', 'project.infrastructure.images',
-              'project.infrastructure.images.main'],
+    packages=find_packages(),
     package_dir={'': 'src'},
     url='https://github.com/ikerRAD/VAE-TFG.git',
     license='MIT',
